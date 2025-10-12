beada72d2f64fcf544ce8940c499590c5dc7a1a5
meluni9 <melnika243@gmail.com>
2025-10-12 21:36:29 +0300

    feat: add date to healthcheck

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from flask import jsonify
 from app import app
+from datetime import datetime
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    return jsonify({"status": "ok"}), 200
+    return jsonify({
+        "status": "ok",
+        "date": datetime.now().isoformat()
+    }), 200
