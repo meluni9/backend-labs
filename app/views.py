e6928706ad67b89a367c5d9aca66b9e7b60d563c
meluni9 <melnika243@gmail.com>
2025-10-12 21:36:53 +0300

    feat: add welcome endpoint

--- app/views.py
+++ app/views.py
@@ -2,6 +2,12 @@ from flask import jsonify
 from app import app
 from datetime import datetime
 
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify({
+        "message": "Welcome!",
+    }), 200
+
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     return jsonify({
