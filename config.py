d32ce9e9be9a2d24821277d46cab4f0da50afdec
meluni9 <melnika243@gmail.com>
2025-12-19 03:15:27 +0200

    chore: add jwt configuration and dependencies

--- config.py
+++ config.py
@@ -12,3 +12,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///data.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
