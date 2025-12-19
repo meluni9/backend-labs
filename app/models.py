5ef71482a8f6424038740645d58d4058e049e737
meluni9 <melnika243@gmail.com>
2025-12-19 02:01:06 +0200

    feat: add ORM models including variant logic for user-specific categories

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from app import db
+from sqlalchemy.sql import func
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    records = db.relationship("Record", back_populates="user", cascade="all, delete")
+    categories = db.relationship("Category", back_populates="user")
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+
+    user = db.relationship("User", back_populates="categories")
+    records = db.relationship("Record", back_populates="category", cascade="all, delete")
+
+
+class Record(db.Model):
+    __tablename__ = "records"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    created_at = db.Column(db.TIMESTAMP, server_default=func.now())
+    amount = db.Column(db.Float(precision=2), nullable=False)
+
+    user = db.relationship("User", back_populates="records")
+    category = db.relationship("Category", back_populates="records")
