691e061bd1c40a6719249c647b2323096e8f07f2
meluni9 <melnika243@gmail.com>
2025-12-19 03:17:03 +0200

    feat: add password field to user model

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
     records = db.relationship("Record", back_populates="user", cascade="all, delete")
     categories = db.relationship("Category", back_populates="user")
